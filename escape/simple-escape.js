02b5647f263531eb133f703d20e4f9258bc590e4
Sean Chen <sean.chen@leocorn.com>
2019-01-18 18:16:28 +0000

    get started the escape game.

---
+++ escape/simple-escape.js
@@ -0,0 +1,28 @@
+/**
+ * the Vue apps from Vue.js introduction.
+ */
+
+/**
+ * the very first app, just render data to DOM by using
+ * the straightforward template syntax.
+ *  {{message}}
+ * 
+ * the syntax to load template.
+ */
+var app = new Vue({
+  el: '#app',
+  data: {
+    ready: 'none',
+    staticMessage: 'Hello Vue.js!',
+
+    usaPresent: '',
+  },
+  // computed will binding dynamic data, it just like 
+  // data, but could use function.
+  computed: {
+    dynamicMessage: function() {
+      return "I am from a function";
+    }
+  }
+});
+
