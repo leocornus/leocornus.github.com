05fc550fe362981705141e68f56fb50d3d9a2aa2
Sean Chen <sean.chen@leocorn.com>
2019-03-04 14:44:30 -0500

    add the bash script to generate index.html

---
+++ bootstrap/generateIndex.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'Start to generate the index.html file for this folder...'
+
+echo ' 
+<!DOCTYPE html>
+<html><body>
+<ul>' > index.html
+
+HTMLS=*.html
+#HTMLS=find . -name '*.html'
+
+#for file in `find . -name '*.html'`
+#for file in '*.html'
+for file in $HTMLS
+do 
+    echo "<li><a href='$file'>$file</a></li>" >> index.html
+done
+
+echo '
+</ul>
+</body></html>' >> index.html
+
+echo "<!-- Generated on $(date) -->" >> index.html
+
+echo 'File generated!'
+echo '============'
+cat index.html
